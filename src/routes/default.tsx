8905cadec4d3a1c161f0152bf271feaa15be5c54
Daniel Silva <dfsilva.dxp@gmail.com>
2024-01-16 18:42:37 -0300

    feat: importa as páginas home, sign in e sign up.

--- src/routes/default.tsx
+++ src/routes/default.tsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 
-import { endpoint } from "@/utils/common/constant/endpoints";
+import { HomePage, SignInPage, SignUpPage } from "@/pages";
+
+import { ENDPOINT } from "@/utils/common/constant/endpoints";
 
 const DefaultRoutes = () => {
   return (
     <Routes>
-      <Route path={endpoint.HOME} element={<p>Home</p>} />
-      <Route path={endpoint.SIGNIN} element={<p>Sign in</p>} />
-      <Route path={endpoint.SIGNUP} element={<p>Sign up</p>} />
-      <Route path={endpoint.PROFILE.HOME}>
+      <Route path={ENDPOINT.HOME} element={<HomePage />} />
+      <Route path={ENDPOINT.SIGNIN} element={<SignInPage />} />
+      <Route path={ENDPOINT.SIGNUP} element={<SignUpPage />} />
+      <Route path={ENDPOINT.PROFILE.HOME}>
         <Route index element={<p>Profile</p>} />
-        <Route path={endpoint.PROFILE.EDIT} element={<p>Profile / Edit</p>} />
+        <Route path={ENDPOINT.PROFILE.EDIT} element={<p>Profile / Edit</p>} />
       </Route>
     </Routes>
   );
